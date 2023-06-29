ccbfe8ee1faf99e85fe90b18d718176fb343dcff
IgorV <vertaiev@gmail.com>
2023-06-29 15:06:50 +0300

    make user_handlers.py

--- bot.py
+++ bot.py
@@ -3,19 +3,20 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 
+
 # Функция конфигурирования и запуска бота
 async def main() -> None:
-
     # Загружаем конфиг в переменную config
-    config: Config= load_config()
+    config: Config = load_config()
 
     # Инициализируем бот и диспетчер
-    bot: Bot=Bot(token=config.tg_bot.token)
-    dp: Dispatcher=Dispatcher()
+    bot: Bot = Bot(token=config.tg_bot.token)
+    dp: Dispatcher = Dispatcher()
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     asyncio.run(main())
